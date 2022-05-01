d774a2532447e22e974969dc6c51b9ca5a3b1ab2
Brian Mohammed Catraguna <briancatraguna@rocketmail.com>
2022-05-01 23:50:46 +0200

    Changed activity to fragment

--- feature/src/main/java/com/briancatraguna/feature/MainActivity.kt
+++ feature/src/main/java/com/briancatraguna/feature/MainActivity.kt
@@ -1,51 +1,16 @@
 package com.briancatraguna.feature
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import androidx.activity.viewModels
 import com.briancatraguna.feature.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.apply {
-            vm = viewModel
-            lifecycleOwner = this@MainActivity
-        }
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return if (item.itemId == R.id.share) {
-            val shareIntent = Intent(Intent.ACTION_SEND)
-            shareIntent.apply {
-                putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.share_subject))
-                putExtra(
-                    Intent.EXTRA_TEXT,
-                    resources.getString(R.string.share_text, viewModel.astroPicture.value?.hdUrl)
-                )
-                type = "text/plain"
-            }
-            startActivity(shareIntent)
-            true
-        } else super.onOptionsItemSelected(item)
-
     }
 }
---
+++ feature/src/main/java/com/briancatraguna/feature/PictureOfTheDayFragment.kt
@@ -0,0 +1,39 @@
+package com.briancatraguna.feature
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.briancatraguna.feature.databinding.FragmentPictureOfTheDayBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PictureOfTheDayFragment : Fragment() {
+
+    private var _binding: FragmentPictureOfTheDayBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPictureOfTheDayBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.apply {
+            vm = viewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
